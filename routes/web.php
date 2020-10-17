6d68b5936f1f3dd4d006900d46b1abc376429aeb
taku <tttks157@gmail.com>
2020-10-17 05:42:31 +0000

    Laravel19確認依頼

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,4 @@ Route::group(['prefix' => 'admin', 'middleware' =>'auth'], function(){
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'NewsController@index');
